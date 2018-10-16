ae74cf329b567c699bdab549d3d0378836c622a3
sleeplesseditor <josh@bryson.me.uk>
2018-10-16 01:04:49 +0100

    Setup Webpack loaders configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path'); 
 const config = {
-    entry: path.join(__dirname, '/client/index.js'), 
-    output: { 
-        path: path.join(__dirname, './public/'), 
-        filename: 'bundle.js' 
-    },
-    module: { 
-        rules: [ 
-            {
-                test: /\.(js|jsx)$/, 
-                loader: 'babel-loader' 
-            }
-        ]
-    }
+  entry: path.join(__dirname, '/client/index.js'), 
+  output: { 
+    path: path.join(__dirname, './public/'), 
+    filename: 'bundle.js' 
+  },
+  module: { 
+    rules: [
+      {
+        test: /\.(js|jsx)$/, 
+        loader: 'babel-loader' 
+      },
+      { 
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
 
 module.exports = config;
